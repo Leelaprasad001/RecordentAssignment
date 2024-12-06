9473e0adb718b65ffbfc3f6d41b98aaabe33b3fd
leelaprasad1607 <leelaprasad1607@gmail.com>
2024-12-06 05:43:42 +0000

    buttons fixed

--- frontend/src/Components/Main.jsx
+++ frontend/src/Components/Main.jsx
@@ -11,8 +11,8 @@ const Main = () => {
     <div className='container'>
       <div className="max-w-full text-center mt-20">
         <p className="uppercase text-custom-blue font-semibold">Welcome to Recordent</p>
-        <h2 className="text-5xl mt-5 font-bold">Indias most comprehensive credit and collection <br /> management platform</h2>
-        <div className='flex justify-center mt-10 gap-4'>
+        <h2 className="text-2xl md:text-4xl mt-5 font-bold">Indias most comprehensive credit and collection <br /> management platform</h2>
+        <div className='flex flex-col sm:flex-row justify-center mt-10 gap-4'>
           <button
             onClick={openModal}
             className="px-6 py-2 border border-custom-blue bg-custom-blue text-neutral-100 rounded-3xl hover:bg-white hover:text-custom-blue"
@@ -20,7 +20,7 @@ const Main = () => {
             Add Employee Data
           </button>
           <a href="/signup" className="px-6 py-2 border border-custom-orange bg-custom-orange text-neutral-100 rounded-3xl hover:bg-white hover:text-custom-orange">
-            View Data
+            View Employee Data
           </a>
         </div>
       </div>
