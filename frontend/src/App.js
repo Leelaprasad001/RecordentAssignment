1f8886101040887dee7f42fb8108af4873bb92ac
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2026-04-20 17:38:47 +0530

    Remove CORS test API call from App component
    Removed unused CORS test API call from useEffect.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
-// App.js
-
-import { Suspense, lazy, useEffect } from 'react';
+import { Suspense, lazy } from 'react';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainerStyled } from './Utils/Toasts';
 import { AuthProvider, useAuth } from './Utils/AuthContext';
@@ -14,37 +12,10 @@ const SignUp = lazy(() => import('./Components/SignUp'));
 const App = () => {
   const { auth } = useAuth();
 
-  useEffect(() => {
-    const testCors = async () => {
-      try {
-        const response = await fetch(
-          'https://www.api-dev.recordent.com/RecordentSummary?reqData=ch1buu5RqawVS3c8ckSuS2uRx2fbV3k0xBs1VxBX3PE%253D',
-          {
-            method: 'GET',
-            credentials: 'include'
-          }
-        );
-
-        console.log('API STATUS:', response.status);
-        console.log('CORS Allowed ✅');
-
-        const data = await response.json();
-        console.log('API RESPONSE:', data);
-
-      } catch (error) {
-        console.error('CORS Blocked ❌');
-        console.error(error);
-      }
-    };
-
-    testCors();
-  }, []);
-
   return (
     <Suspense fallback={<Loading />}>
       <Header />
       <ToastContainerStyled />
-
       <Routes>
         <Route path="/" element={auth ? <Main /> : <Navigate to="/signin" />} />
         <Route path="/signin" element={!auth ? <SignIn /> : <Navigate to="/" />} />
